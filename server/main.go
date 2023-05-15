55f5ac2034d2cc20de8984e9e0f19d394c012341
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-05-15 15:57:32 +0300

    server.go

--- server/main.go
+++ server/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"net"
+	"log"
+	"google.golang.org/grpc"
+	pb "github.com/alvo254/microverse/proto"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct{
+	pb.GreetServiceServer
+}
+
+func main() {
+	listen, error := net.Listen("tcp", port)
+	if error != nil{
+		log.Fatalf("Failed to start the server %v", error)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("Server started at %v", listen.Addr())
+	if error := grpcServer.Serve(listen); error != nil {
+		log.Fatalf("Failed to start %v", error)
+	}
+}
